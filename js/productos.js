8d887274f1f185f15e8c0cf7140ce00733d64846
Guido Abulafia <guidoabula@gmail.com>
2025-11-11 15:50:49 -0300

    Agrergado de JSs

---
+++ js/productos.js
@@ -0,0 +1,39 @@
+//⚠️Detalle: rutas ABSOLUTAS ya que ahora se usaran desde 2 html en distintas ubicaciones
+export const productos = [
+  {
+    id: "1",
+    nombre: "Harry Potter: El prisionero de Azkaban",
+    img: "img/HarryPotterPrisionero.jpg",
+    precio: 15,
+  },
+  {
+    id: "2",
+    nombre: "Trilogia Mistborn Brandon Sanderson",
+    img: "img/Trilogia Misborn.jpg",
+    precio: 80,
+  },
+  {
+    id: "3",
+    nombre: "El señor de los anillos + El hobbit",
+    img: "img/hobbitylotr.jpg",
+    precio: 20,
+  },
+  {
+    id: "4",
+    nombre: "Trilogia del Archivo de las tormentas",
+    img: "img/SandersonArchivo.jpg",
+    precio: 20,
+  },
+  {
+    id: "5",
+    nombre: "Paquete Agatha Cristie",
+    img: "img/AgathaCriestie.jpg",
+    precio: 40,
+  },
+  {
+    id: "5",
+    nombre: "Paquete Percy Jackson",
+    img: "img/Percy Jackson.jpg",
+    precio: 40,
+  },
+];
